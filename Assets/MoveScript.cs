bff8c4a85388abd9e2657446e28685e2b25dd186
Brian <bgeiger@csumb.edu>
2019-06-17 05:07:50 -0700

    moving from drive to git

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + .01f);
+
+		if (Input.GetMouseButton (0)) {
+			rb.AddForce (0, 20, 20);
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/RandomWalkControler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWalkControler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
